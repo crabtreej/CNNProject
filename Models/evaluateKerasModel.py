25e014bccd1f8e1d1c532bd5a21a0f35185835e3
Jacob Crabtree <crabtree.234@buckeyemail.osu.edu>
2019-04-06 13:33:30 -0400

    Accidentally pushed nothing. Keras code now saves training/validation error as pickle. Added utility to evaluateKerasModel.py to also display training results in model's pickle file.

--- Models/evaluateKerasModel.py
+++ Models/evaluateKerasModel.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import pickle
 
 # Jacob - takes a path to a model saved in a .h5 file 
 # and returns a keras model that is ready for evaluation on a test set
@@ -30,6 +31,30 @@ def report_test_metrics(model, x_test, y_test):
     print('Test loss:', scores[0])
     print('Test accuracy:', scores[1])
 
+# Jacob - This iterates through each epoch in the training history
+# saved in the pickle file, and prints the validation and training
+# accuracy. 
+def report_training_history(train_hist_path):
+    # Jacob - Read in the file
+    train_hist_file = open(train_hist_path + '.pckl', 'rb')
+    hist_dict = pickle.load(train_hist_file)
+    train_hist_file.close()
+
+    # Jacob - Get each list in the dictionary
+    val_loss_hist = hist_dict['val_loss']
+    train_loss_hist = hist_dict['loss']
+    val_acc_hist = hist_dict['val_acc']
+    train_acc_hist = hist_dict['acc']
+
+    # Jacob - Zip everything together and then print out the stats.
+    # Now that we pickle all the training results too, we can just
+    # come back at some later time to actually make graphs and such
+    num_epochs = len(val_loss_hist)
+    print(f'Training Stats per Epoch. {num_epochs} total epochs.')
+    epoch = 1
+    for val_loss, train_loss, val_acc, train_acc in zip(val_loss_hist, train_loss_hist, val_acc_hist, train_acc_hist):
+        print(f'Epoch {epoch} / {num_epochs}: \nValidation Loss: {val_loss:.4f}, Validation Accuracy: {val_acc:.4f}\nTraining Loss: {train_loss:.4f}, Training Accuracy: {train_acc:.4f}\n')
+        epoch += 1
 
 # Jacob - use argparse to get the path to the model, load in test data, and then
 # evaluate the model on the test data. If you have an already existing model and you
@@ -39,11 +64,12 @@ if __name__ == '__main__':
 
     # Jacob - using argparse just to make this nice to use
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model_name', required=True, help='File name of saved model, include .h5')
+    parser.add_argument('-m', '--model_name', required=True, help='File name of saved model, not including extension')
     parser.add_argument('--model_dir', default=default_model_dir, help='Specify if model isn\'t in ./saved_models/')
+    parser.add_argument('-t', '--show_training', action='store_true', help='Specify if you want to see the training history (validation error, testing error, and loss)')
     args = parser.parse_args()
 
-    model_path = os.path.join(args.model_dir, args.model_name)
+    model_path = os.path.join(args.model_dir, args.model_name + '.h5')
 
     # Jacob - these take forever to import, so there's no reason to
     # do it until we know we need them (e.g. someone could specify -h for arg).
@@ -64,3 +90,6 @@ if __name__ == '__main__':
 
     model = reload_keras_model(model_path)
     report_test_metrics(model, x_test, y_test)
+
+    if args.show_training:
+        report_training_history(os.path.join(args.model_dir, args.model_name))
